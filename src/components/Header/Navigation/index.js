9509ef9912797c79a8fc7337b22ba3814d7259aa
esraamustafahh <esraa.msuatafhhh@gmail.com>
2026-07-11 21:04:40 +0300

    the dropDown is done!

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -17,16 +17,33 @@ const Navigation = () =>{
                     <div className='container'>
                         <div className='row'>
                             <div className='col-sm-3 navPart1'>
-                                <Button className='allCatTab align-items-center'>
+                                <div className="catWrapper">
+                                 <Button className='allCatTab align-items-center'>
                                     <span className="icon1 mr-2"><RxHamburgerMenu /></span>
                                     <span className='text'>ALL CATEGORIES</span>
                                     <span className="icon2 ml-4"><FaAngleDown /></span>
-                                </Button>
+                                 </Button>
+                                 <div className="sideBarNav shadow">
+                                    <ul>
+                                    <li className="list-inline-item"><Link to="/"><Button><IoMdHome />Home</Button></Link></li>
+                                    <li className="list-inline-item"><Link to="/"><Button><MdFoodBank />Meats and Food</Button></Link></li>
+
+                                    </ul>
+                                 </div>
+                                </div>
+                              
                             </div>
                         <div className='col-sm-9 navPart2 d-flex align-items-center'>
                                 <ul className="list list-inline ml-auto">
                                     <li className="list-inline-item"><Link to="/"><Button><IoMdHome />Home</Button></Link></li>
-                                    <li className="list-inline-item"><Link to="/"><Button><MdFoodBank />Meats and Food</Button></Link></li>
+                                    <li className="list-inline-item"><Link to="/"><Button><MdFoodBank />Meats and Food</Button></Link>
+                                    <div className="submenu shadow">
+                                    <Link to="/"><Button>Cakes</Button></Link>
+                                    <Link to="/"><Button>Cupcakes</Button></Link>
+                                    <Link to="/"><Button>Tre leche</Button></Link>
+                                    <Link to="/"><Button>Gatoh</Button></Link>
+                                    </div>
+                                    </li>
                                     <li className="list-inline-item"><Link to="/"><Button><MdOutlineBakeryDining />Bakery</Button></Link></li>
                                     <li className="list-inline-item"><Link to="/"><Button><GiWrappedSweet />snacks</Button></Link></li>
                                     <li className="list-inline-item"><Link to="/"><Button><MdElectricalServices/>electronics</Button></Link></li>
