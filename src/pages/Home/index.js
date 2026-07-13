445ffd11219109700aec897b8af9d0f9401f9e75
esraamustafahh <esraa.msuatafhhh@gmail.com>
2026-07-13 11:41:58 +0300

    bootstrap best seller is done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,7 +23,7 @@ return(
             <div className="row">
                 <div className="col-md-3">
                     <div className="banner">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyeq3YxzpqATCJAlDX5icD0hvkl1NQPyHLk0a68IO90MCXcTlMvqtJSA&s=10" className="cursor w-100"/>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_xC-iqFlaeeeTP5ahMUJ1J5KVx4QNX9CiLK08c5PW31C8uKt6OzkxuEr_&s=10" className="sideImg cursor w-100"/>
                     </div>
                 </div>
                  <div className="col-md-9">
@@ -38,42 +38,66 @@ return(
                     </div>
                     <div className="productRow w-100 mt-3">
                         <Slider {...productSliderOptions}>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
-                            <div className="item productItem">
-                                <div className="imgWrapper">
-                                    <img className="w-100" src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" />
-                                </div>
-                       
-                            </div>
+                          
+                            <div className="card card-right" style="width: 18rem;">
+  <img src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1752334763_9f5fb91b-e92b-470d-87be-3f667d80f6f7.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Chessy Mushroom</h5>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">in stock</li>
+    <li className="list-group-item text-secondary">190 LE</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="btn btn-card w-100 card-link">Read More</a>
+  </div>
+</div>
+
+
+ <div className="card card-middle" style="width: 18rem;">
+  <img src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1767202250_a0b9e23d-0d20-4cba-b02c-f5f9e60449a5.jpg
+" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Chessy Mushroom</h5>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">in stock</li>
+    <li className="list-group-item text-secondary">190 LE</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="btn btn-card w-100 card-link">Read More</a>
+  </div>
+</div>
+
+
+ <div className="card card-middle" style="width: 18rem;">
+  <img src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1701100860_9ab6d740-1a26-455d-9765-dbd23efa1175.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Chessy Mushroom</h5>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">in stock</li>
+    <li className="list-group-item text-secondary">190 LE</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="btn btn-card w-100 card-link">Read More</a>
+  </div>
+</div>
+
+ <div className="card card-left" style="width: 18rem;">
+    <img src="https://foodics-console-production.s3.eu-west-1.amazonaws.com/images/585174_1739978869_9e400a3e-93d6-44fa-b9fd-847a4ceea88d.jpeg" className="card-img-top"/>
+  <div className="card-body">
+    <h5 className="card-title">Chessy Mushroom</h5>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">in stock</li>
+    <li className="list-group-item text-secondary">190 LE</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="btn btn-card w-100 card-link">Read More</a>
+  </div>
+</div>                      
+                            
                        
                         </Slider>
                     </div>
