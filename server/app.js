dc538af920deee8845256d7bbc0e7a03c39ab679
esraamustafahh <esraa.msuatafhhh@gmail.com>
2026-07-30 11:36:00 +0300

    fixing postman errors and added some routes

--- server/app.js
+++ server/app.js
@@ -1,26 +1,45 @@
-const express = require();
+const express = require('express');
 const app=express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors=require('cors');
+const categoryRoutes=require('./routes/category');
+const plimit = require('p-limit')
 require('dotenv/config');
 
 app.use(cors());
-app.options('*',cors());
+// app.options('/*splat',cors());
 
-mongoose.connect(process.env.CONNECTION_STRING,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(() =>{
-    console.log('database is working')
-})
-.catch((e) =>{
-    console.log('error:' , e)
-})
+main().catch(err => {
+    console.log(err);
+    console.log('there is an errorr');
+});
+async function main() {
+    await mongoose.connect('mongodb://yasminealyy:yasmina12@ac-slz8z22-shard-00-00.nuxgayy.mongodb.net:27017,ac-slz8z22-shard-00-01.nuxgayy.mongodb.net:27017,ac-slz8z22-shard-00-02.nuxgayy.mongodb.net:27017/e-commerce?ssl=true&replicaSet=atlas-11c28c-shard-0&authSource=admin&appName=Cluster0'
+
+    )
+
+    console.log('connected')
+}
+// mongoose.connect(process.env.CONNECTION_STRING,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
+// .then(() =>{
+//     console.log('database is working')
+// })
+// .catch((e) =>{
+//     console.log('error:' , e)
+// })
 app.use(bodyParser.json());
 
+app.use(`/api/category` , categoryRoutes);
+
+app.post('/test', (req, res) => {
+    console.log("TEST ROUTE HIT");
+    res.json({ message: "Working" });
+});
 
 app.listen(process.env.PORT ,()=>{
-    console.log('is running')
+    console.log(`is running on ${process.env.PORT}`)
 })
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,5 +1,8 @@
+const { Promise } = require('mongoose');
+const { cloudinary } = require('../cloudinary');
 const {Category} = require('../models/category');
 const express = require('express');
+const pLimit = require('p-limit');
 const router = express.Router();
 
 
@@ -9,6 +12,70 @@ router.get('/' , async(req,res) =>{
         res.status(500).json({success:false})
     }
     res.send(categoryList);
+})
+
+router.get('/:id' , async(req,res) =>{
+const category =  await Category.findById(req.params._id);
+if(!category){
+    res.status(500).json({message:'The category whith this id is not existing'})
+}
+return res.status(201).send(category);
+})
+
+router.post('/create' , async(req,res) =>{
+    console.log(req.body);
+    const limit = pLimit(3);
+
+    const imagesToUpload = req.body.images.map((img)=>{
+        return limit (async()=>{
+            const result = await cloudinary.uploader.upload(img);
+            return result;
+        })
+    })
+})
+
+router.delete('/:id' , async(req,res) =>{
+    const deletedUser = await Category.findByIdAndRemove(req.params._id);
+
+    if(!deletedUser){
+        res.status(404).json({
+            message:'category not found',
+            success:false
+        })
+    }
+    res.status(201).json({
+        message:'category is deleted',
+        success:true
+    })
+
+const uploadStatus = await Promise.all(imagesToUpload);
+
+const imgUrl = uploadStatus.map((item)=>{
+    return item.secure_url
+})
+
+if(!uploadStatus){
+    return res.status(500).json({
+        error:'images cannot be uploaded',
+        status:false
+    })
+}
+
+let category = new Category({
+    name:req.body.name,
+    images:imgUrl
+})
+
+if(!category){
+    res.status(500).json({
+        error:err,
+        status:false
+    })
+}
+
+category =await category.save();
+
+res.status(201).json(category);
 })
 
 module.exports=router;
